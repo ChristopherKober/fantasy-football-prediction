10498c323412e5dc2956b9c3f913a742a899fc23
Christopher Kober <ChristopherKober@users.noreply.github.com>
2020-04-04 23:23:55 -0400

    Add generic component framework

---
+++ src/fantasy-football-webapp/src/common/navpanel/navpanel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class NavPanel extends React.Component {
+    render() {
+        return <h1>NavPanel</h1>;
+    }    
+}
---
+++ src/fantasy-football-webapp/src/common/titlebar/titlebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './titlebar.scss'
+
+export class TitleBar extends React.Component {
+    render() {
+        return <div class="titlebar">
+            <body>TitleBar</body>
+        </div>;
+    }    
+}
